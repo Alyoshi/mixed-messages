a5f78f14b9478da58cbe43f8c7a76f76f4039536
COMPUTER\basti <bastian.sattelberger@gmail.com>
2022-05-22 14:52:20 +0200

    update to do

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ let messagesObject ={
         this._messages = inputMessages;
     },
     getRandomMessage(){
+        //todo: use at least 3 sources for a stupid story and build a string of it, which will be returned (like goat, castle and boat)
         let randomIndex = Math.floor(Math.random()*this._messages.length);
         return this._messages[randomIndex];
     }
