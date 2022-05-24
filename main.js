797ba7b9a4edb286508c28c991b4a81fd9f7e8e1
COMPUTER\basti <bastian.sattelberger@gmail.com>
2022-05-24 21:35:58 +0200

    update

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
+let randomIndex = -1;
+let randomIndexCompare = -1;
+
 let messagesObject ={
     _messages: ['France', 'Italy', 'Bavaria','South Africa'],
-    set messages(inputMessages){
-        this._messages = inputMessages;
+    get messages(){
+        return this._messages;
     },
     getRandomMessage(){
         //todo: use at least 3 sources for a stupid story and build a string of it, which will be returned (like goat, castle and boat)
-        let randomIndex = Math.floor(Math.random()*this._messages.length);
+        randomIndexCompare = randomIndex;
+        randomIndex = Math.floor(Math.random()*this._messages.length);
+        while(randomIndex === randomIndexCompare){
+            randomIndex = Math.floor(Math.random()*this._messages.length);
+        }
         return this._messages[randomIndex];
     }
 };
@@ -16,7 +23,7 @@ elem.textContent = messagesObject.getRandomMessage();
 }
 
 // messagesObject.messages = ['A', 'B', 'C', 'D', 'E', 'F'];
-// console.log(messagesObject.getRandomMessage());
+console.log(messagesObject.getRandomMessage());
 
 /*
 1. Now that’s what I call stupid: In my junior year of high school, this guy asked me on a date. He rented a Redbox movie and made a pizza. We were watching the movie and the oven beeped so the pizza was done. He looked me dead in the eye and said, “This is the worst part.” I then watched this boy open the oven and pull the pizza out with his bare hands, rack and all, screaming at the top of his lungs. We never had a second date.
