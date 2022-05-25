36915cab9f51abd3fbe77dd6de212d9be56d83e3
COMPUTER\basti <bastian.sattelberger@gmail.com>
2022-05-25 21:03:14 +0200

    fixed jumbotron

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ let randomIndexCompare = -1;
 
 let messagesObject ={
     _messages: ['France', 'Italy', 'Bavaria','South Africa'],
-    get messages(){
-        return this._messages;
+    set messages(inputMessage) {
+        this._messages = inputMessage;
     },
     getRandomMessage(){
         //todo: use at least 3 sources for a stupid story and build a string of it, which will be returned (like goat, castle and boat)
@@ -21,11 +21,3 @@ const updateMessages = () => {
 const elem = document.getElementById('story-text');
 elem.textContent = messagesObject.getRandomMessage();
 }
-
-// messagesObject.messages = ['A', 'B', 'C', 'D', 'E', 'F'];
-console.log(messagesObject.getRandomMessage());
-
-/*
-1. Now that’s what I call stupid: In my junior year of high school, this guy asked me on a date. He rented a Redbox movie and made a pizza. We were watching the movie and the oven beeped so the pizza was done. He looked me dead in the eye and said, “This is the worst part.” I then watched this boy open the oven and pull the pizza out with his bare hands, rack and all, screaming at the top of his lungs. We never had a second date.
-bare hands, cat, 
-*/
